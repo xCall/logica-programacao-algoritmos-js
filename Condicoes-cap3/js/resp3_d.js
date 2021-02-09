79a647364b5fe9d094188b671ebe9518ea6c2d1e
xCall <carlosfaita99@gmail.com>
2021-02-09 00:08:29 -0300

    resolvido exercicio jutamente com erro de lógica

--- Condicoes-cap3/js/resp3_d.js
+++ Condicoes-cap3/js/resp3_d.js
@@ -8,11 +8,26 @@ function Verificar() {
     var ladoA = Number(inLadoA.value);
     var ladoB = Number(inLadoB.value);
     var ladoC = Number(inLadoC.value);
+    console.log(ladoB)
 
-    if ((ladoA == "" || isNaN(ladoA)) || (ladoB == "" || isNaN(ladoB)) || (ladoC == "" || isNaN(ladoC))) {
+    if ((ladoA == 0 || isNaN(ladoA)) || (ladoB == 0 || isNaN(ladoB)) || (ladoC == 0 || isNaN(ladoC))) {
         outLados.textContent = `Para continuar preencha os campos com valores válidos.`;
         return;
     }
 
-    if
+    if (ladoA < ladoB + ladoC || ladoB < ladoA + ladoC || ladoC < ladoA + ladoB) {
+        outLados.textContent = `Podem formar um triângulo`;
+        if ((ladoA == ladoB) && (ladoA == ladoC)) {
+            outTipo.textContent = `Tipo: Equilátero`;
+        } else if ((ladoA == ladoB) || (ladoB == ladoC) || (ladoA == ladoC)) {
+            outTipo.textContent = `Tipo: Isóceles`
+        } else if (((ladoA != ladoB) != ladoC) || ((ladoB != ladoC) != ladoA) || ((ladoA != ladoC) != ladoB)) {
+            outTipo.textContent = `Tipo: Escaleno`;
+        }
+        return;
+    } else {
+        outTipo.textContent = `Impossivel formar um triângulo`
+    }
 }
+
+var btVerificar = document.getElementById('btVerificar').addEventListener('click', Verificar);
